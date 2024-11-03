5f9c6d19ec1aa741a1039a66ebc333e26e4eb71b
cironis <ciroshia@gmail.com>
2024-11-03 21:26:08 +0000

    Updating app.py

--- auxiliar.py
+++ auxiliar.py
@@ -6,11 +6,11 @@ import random
 def create_sample_df(sample_size):
     # Sample data for each column based on the description
     data = {
-        "Gênero": [random.choice(["Masculino", "Feminino", "Outros", "-"]) for _ in range(sample_size)],
+        "Gênero": [random.choice(["Masculino", "Feminino", "Outros"]) for _ in range(sample_size)],
         "Idade": [random.randint(18, 30) for _ in range(sample_size)],
         "Ano de ingresso": [random.choice(["Antes de 2020", "2020", "2021", "2022", "2023", "2024"]) for _ in range(sample_size)],
-        "Instituto": [random.choice(["IME", "IF", "-"]) for _ in range(sample_size)],
-        "Curso": [random.choice(["Licenciatura", "Bacharelado", "-"]) for _ in range(sample_size)],
+        "Instituto": [random.choice(["IME", "IF"]) for _ in range(sample_size)],
+        "Curso": [random.choice(["Licenciatura", "Bacharelado"]) for _ in range(sample_size)],
         "Quanto tempo você esta na USP": [random.randint(1, 5) for _ in range(sample_size)],
         "Qualidade das aulas": [random.randint(0, 5) for _ in range(sample_size)],
         "Infraestrutura física": [random.randint(0, 5) for _ in range(sample_size)],
