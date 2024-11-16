d1fe5fdd947e691d66c21ca2e889f49bd17948f5
cironis <ciroshia@gmail.com>
2024-11-16 20:33:54 +0000

    Updating app.py

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ dados_coletados = st.Page(
 
 analisar_dados = st.Page(
     "views/analisar_dados.py",
-    title="Análise de satisfação (sample data)",
+    title="Análise de satisfação",
     icon=":material/overview:",
 )
 
 caracteristicas = st.Page(
     "views/caracteristicas.py",
-    title="Características da População (sample data)",
+    title="Características da População",
     icon=":material/overview:",
 )
 
