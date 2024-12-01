bc80944caa18374b81bf6afde9557d568f608706
cironis <ciroshia@gmail.com>
2024-12-01 20:58:49 +0000

    Updating app.py

--- app.py
+++ app.py
@@ -2,11 +2,31 @@ import streamlit as st
 
 # --- PAGE SETUP ---
 
+home = st.Page(
+    "views/home.py",
+    title="Introdução",
+    icon=":material/overview:",
+    default=True,
+)
+
+
+descricao = st.Page(
+    "views/descricao.py",
+    title="Descrição",
+    icon=":material/overview:",
+)
+
+analise_independencia = st.Page(
+    "views/analise_independencia.py",
+    title="Análise de Independência",
+    icon=":material/overview:",
+)
+
+
 dados_coletados = st.Page(
     "views/dados_coletados.py",
     title="Dados Coletados",
-    icon=":material/overview:",
-    default=True,
+    icon=":material/overview:"
 )
 
 analisar_dados = st.Page(
@@ -20,11 +40,7 @@ caracteristicas = st.Page(
     title="Características da População",
     icon=":material/overview:",
 )
-analise_independencia = st.Page(
-    "views/analise_independencia.py",
-    title="Análise de Independência",
-    icon=":material/overview:",
-)
+
 
 
 # --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
@@ -33,7 +49,7 @@ analise_independencia = st.Page(
 # --- NAVIGATION SETUP [WITH SECTIONS]---
 pg = st.navigation(
     {
-        "Visualização": [dados_coletados,caracteristicas,analisar_dados,analise_independencia]
+        "Visualização": [home,dados_coletados,caracteristicas,analisar_dados,analise_independencia]
     }
 )
 
