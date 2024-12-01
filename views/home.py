e53735dded3c8e106dbf4f2d1f7f83f0f6f710c7
cironis <ciroshia@gmail.com>
2024-12-01 21:17:51 +0000

    Updating app.py

--- views/home.py
+++ views/home.py
@@ -9,5 +9,41 @@ import plotly.graph_objects as go
 
 st.set_page_config(page_title="Introdução", page_icon="📊", layout="wide")
 
+st.tittle("Título ")
+
 st.title("Tabela do Grupo")
 
+dados_do_grupo = {
+                  
+                  }
+
+# Data for the group
+data = {
+    "Foto": ["assets/Bruna.enc",
+                "assets/Ciro.enc",
+                "assets/Enzo.enc",
+                "assets/Gustavo.enc",
+                "assets/Icaro.enc",
+                "assets/Mateus.enc"], 
+    "Nome": ["Bruna Hellmeister Bugari",
+             "Ciro Nogueirão Shia",
+             "Enzo Massato Kuniyoshi",
+             "Gustavo Pedro Simplício Correa",
+             "Ícaro Maringelli",
+             "Mateus Eiji Miyazaki Pelegrina"
+             ],
+    "Número USP": ["11223041", "5158197", "13689712", "14577107","XXXXXXXXX"],
+}
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+
+# Display the table with image column configuration
+st.markdown("### Tabela do Grupo")
+st.dataframe(
+    df,
+    column_config={
+        "Foto": st.column_config.ImageColumn("Foto", width="small"),
+    },
+    use_container_width=True,
+)
